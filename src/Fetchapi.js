1b1654b2d125b53bd8c2bef05b36967c869de30f
Parshva <codewithpasshu@gmail.com>
2025-02-23 16:56:39 +0530

    apis

--- src/Fetchapi.js
+++ src/Fetchapi.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
-export default function ApiComp(){
-    const [data,setData]=useState([]);
-    const apiGet = () => {
-        fetch("https://api.rootnet.in/covid19-in/stats/history")
-        .then((response) => response.json())
-        .then(
-            (response) => {setData(response.data[2].day)}
-        )
-    }
-    console.log(data)
-    return(
-        <>
-            <h1>Api data is : {data}</h1>
-            <button onClick={apiGet}>fetch</button>
-        </>
-    );
+export default function ApiComp() {
+  const [data, setData] = useState([]);
+  const apiGet = () => {
+    fetch("https://api.rootnet.in/covid19-in/stats/history")
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.data[2].day);
+      });
+  };
+  console.log(data);
+  return (
+    <>
+      <h1>Api data is : {data}</h1>
+      <button onClick={apiGet}>fetch</button>
+    </>
+  );
 }
