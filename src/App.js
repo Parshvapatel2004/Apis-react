e22ac28ef9577a0bddcc92f948c8966bb99eaf51
Parshva <codewithpasshu@gmail.com>
2025-02-23 22:42:21 +0530

    fkjg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ApiComp from './Fetchapi';
+import Fetchapi from './Fetchapi';
 
 
 function App() {
   return (
     <>
-    <ApiComp/>
+    <Fetchapi/>
     </>
   );
 }
--- src/Fetchapi.js
+++ src/Fetchapi.js
@@ -1,6 +1,20 @@
 import { useState } from "react";
 
-export default function ApiComp() {
+import React from "react";
+
+const Fetchapi = () => {
+  return (
+    <div>
+      {/* <ApiComp /> */}
+      <TableData/>
+      
+    </div>
+  );
+};
+
+export default Fetchapi;
+
+function ApiComp() {
   const [data, setData] = useState([]);
   const apiGet = () => {
     fetch("https://api.rootnet.in/covid19-in/stats/history")
@@ -19,3 +33,55 @@ export default function ApiComp() {
 }
 
 
+function TableData() {
+  const [data, setData] = useState([]);
+  const apiGet = () => {
+    fetch("https://api.rootnet.in/covid19-in/stats/history")
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.data[0].regional);
+      });
+    console.log(data);
+  };
+  return (
+    <center style={{ margin: "50px" }}>
+      <table border={1} cellPadding={5}>
+        <thead>
+          <tr>
+            <th>SR no</th>
+            <th>City</th>
+            <th>ConfirmedCasesIndian</th>
+            <th>ConfirmedCasesForeign</th>
+            <th>Discharged</th>
+            <th>Deaths</th>
+            <th>TotalConfirmed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 ? (
+            data.map((element, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{element.loc}</td>
+                  <td>{element.confirmedCasesIndian}</td>
+                  <td>{element.confirmedCasesForeign}</td>
+                  <td>{element.discharged}</td>
+                  <td>{element.deaths}</td>
+                  <td>{element.totalConfirmed}</td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan={20} style={{ textAlign: "center" }}>
+                No Data
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <button onClick={apiGet}>Fetch </button>
+    </center>
+  );
+}
